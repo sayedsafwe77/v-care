28ea85ebf643da53f03be6db3b0c52c15fe65473
Mahmoud AbouElawad <35901393+abouelawad@users.noreply.github.com>
2025-04-16 13:46:58 +0200

    user registration

--- app/Http/Controllers/DoctorTitleController.php
+++ app/Http/Controllers/DoctorTitleController.php
@@ -8,27 +8,12 @@ use App\Http\Resources\DoctorTitleResource;
 use App\Http\Traits\ApiResponseTrait;
 use App\Models\DoctorTitle;
 use App\Services\DoctorTitleService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Response;
+
 
 class DoctorTitleController extends Controller
 {
-<<<<<<< HEAD
     use ApiResponse,ApiResponseTrait;
-    function index() {
-        $titles = DoctorTitle::get();
-        // return Response::success(DoctorTitleResource::collection($titles));
-        // return response()->success(DoctorTitleResource::collection($titles));
-        // return $this->successResponse(DoctorTitleResource::collection($titles));
-        return $this->ApiResponse(200,'success',null,DoctorTitleResource::collection($titles));
-
-        // return response()->json(['data' => DoctorTitleResource::collection($titles)]);
-=======
-    use ApiResponse;
     public function __construct(public DoctorTitleService $service) {
->>>>>>> a3ea2a45ce435aa5db1333785b703954ad8913be
     }
     function index() {
         return $this->successResponse(DoctorTitleResource::collection($this->service->index()));
